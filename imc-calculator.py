f1ba21986060695bedd26558dfa5ff85f4f25464
Patrick Girardi <111985260+isTrick@users.noreply.github.com>
2023-12-05 23:22:58 -0300

    Update imc-calculator.py
    from 18 lines to 10 lines of code

--- imc-calculator.py
+++ imc-calculator.py
@@ -2,17 +2,9 @@ nome = input('Digite seu nome: ')
 altura = float(input('Digite sua altura: '))
 peso = float(input ('Digite seu peso: '))
 
-
 imc = peso / altura ** 2
 
-if imc >= 18 and imc <= 30: {
+if 18 <= imc <= 30:
     print(f'Parabéns {nome}, o seu IMC é de {imc:.2f}, o que está dentro do ideal!')
-}
-
-if imc < 18: {
-    print(f'{nome}, seu IMC é de {imc:.2f} e está abaixo do ideal.')
-}
-
-if imc > 30: {
-    print(f'{nome}, seu IMC é de {imc:.2f} e está acima do ideal.')
-}
+else:
+    print(f'{nome}, seu IMC é de {imc:.2f} e está fora ideal.')
